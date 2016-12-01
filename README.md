e70f7f65a27c9dcc8c79c800e8a5eeaab4d1cbc5
Shaocong Mo <scottscmo@gmail.com>
2016-12-01 02:01:37 +0000

    Added README, index.html, and index.js for timestamp service

---
+++ README.md
@@ -0,0 +1,11 @@
+# API Basejump: Timestamp microservice
+By Shaocong(Scott) Mo
+This project is for [FreeCodeCamp's Timestamp Microservice Project](https://www.freecodecamp.com/challenges/timestamp-microservice)
+
+## User stories:
+1. I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016)
+2. If it does, it returns both the Unix timestamp and the natural language form of that date.
+3. If it does not contain a date or Unix timestamp, it returns null for those properties.
+
+## Project Site
+https://scottmo-fcc-timestamp.herokuapp.com
---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var path = require("path");
+var moment = require("moment");
+
+var app = express();
+var port = 8080;
+
+app.get('/', function (req, res) {
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent file: ', fileName);
+    }
+  });
+});
+
+app.get('/:dateString', function (req, res) {
+  var date = req.params.dateString;
+  if (isNaN(parseInt(date))) {
+    date = moment(date);
+  } else {
+    date = moment(date, 'X');
+  }
+
+  if (date.isValid()) {
+    res.json({
+      unix: date.format("X"),
+      natural: date.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+});
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port + '!');
+});
